fd5ad374979a3f993f3d2a11af19a6a99b8507ea
MigueBuenoSilva <mbuenosilva60@gmail.com>
2024-01-24 19:34:33 -0300

    Update Chakra

--- My_App/my-app-ts/src/App.tsx
+++ My_App/my-app-ts/src/App.tsx
@@ -1,11 +1,6 @@
 // import { Layout } from './components/Layout';
 // import styled from 'styled-components'
-import { 
-  // Center,
-  ChakraProvider,
-  Box,
-  Input,
-} from '@chakra-ui/react'
+import { Center, ChakraProvider, Box, Input, Button } from "@chakra-ui/react";
 
 // const Box = styled.div`
 //   background-color: orange;
@@ -16,23 +11,31 @@ import {
 function App() {
   return (
     <ChakraProvider>
-      <Box minHeight='100vh' backgroundColor='#004f37'>
-      <h1>Faça seu Login</h1>
-        <Input placeholder= "email"/>
-        <Input placeholder="senha"/>
-      </Box>
-      {/* <Center h='200px'>
-          <Layout>
-          <Box>
+      <Box minHeight="100vh" backgroundColor="#201f30" p="25px" fontSize="16px">
+        <Center>
+          <Box
+            backgroundColor="#f2f2f2"
+            p="15px"
+            borderRadius="10px"
+            w="400px"
+            fontFamily="monospace"
+          >
+            <Center>
+              <h1>Faça seu Login</h1>
+            </Center>
+
+            <Input placeholder="email" mt="10px" />
+            <Input placeholder="senha" mt="10px" />
+            <Button 
+              colorScheme='messenger'
+              w="100%"
+              mt="20px"
+              >
+              Entrar
+            </Button>
           </Box>
-         
-          </Layout>
-        
-        <h1>Faça seu Login</h1>
-        <Input placeholder= "email"/>
-        <Input placeholder="senha"/>    
-        
-      </Center>  */}
+        </Center>
+      </Box>    
     </ChakraProvider>
   );
 }
